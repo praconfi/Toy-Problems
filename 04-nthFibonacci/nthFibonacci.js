caa3bcba20f997e6467e29f5d3311f6d6eb10872
praconfi <praconfi@gmail.com>
2020-10-08 16:35:31 +0900

    [Toy] 4

---
+++ 04-nthFibonacci/nthFibonacci.js
@@ -0,0 +1,35 @@
+/*
+ * 피보나치 수는 첫째 및 둘째 항이 1입니다. 그 뒤의 모든 항은 바로 앞 두 항의 합인 수의 나열입니다.
+ *
+ * 다음과 같은 규칙을 가집니다.
+ *
+ *   1 1 2 3 ...
+ *
+ *
+ * 재귀를 이용하여 피보나치 수열의 n번째 항을 return하는 함수를 작성하세요.
+ *
+ * n이 4로 주어진다면, 작성한 함수는 3을 return해야 하고 5가 주어진다면 5를 return 해야 합니다.
+ *
+ * Advanced : 재귀를 사용하지 않고 iterative하게 함수를 작성하세요.
+ * 참고 : https://techdifferences.com/difference-between-recursion-and-iteration-2.html
+ *
+ * ex)
+ * nthFibonacci(2); // => 1
+ * nthFibonacci(3); // => 2
+ * nthFibonacci(4); // => 3
+ * etc...
+ *
+ */
+
+const nthFibonacci = function (n) {
+  //기반조건
+  if (n === 0) {
+    return 0;
+  }
+  if (n === 1) {
+    return 1;
+  }
+  else {
+    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  }
+};
